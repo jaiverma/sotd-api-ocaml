19bc1b1001bb080800c5c6b02af7648180457317
Jai Verma <jai2.verma@outlook.com>
2022-04-09 16:31:36 +0530

    Example to echo post data

--- bin/main.ml
+++ bin/main.ml
@@ -1,31 +1,11 @@
 open Lwt.Infix
 
-let successful = ref 0
-let failed = ref 0
-
-let count_requests inner_handler request =
-  Lwt.catch
-    (fun () ->
-      inner_handler request
-      >>= fun response ->
-      successful := !successful + 1;
-      Lwt.return response)
-    (fun exn ->
-      failed := !failed + 1;
-      raise exn)
-;;
-
 let () =
   Dream.run
   @@ Dream.logger
-  @@ count_requests
   @@ Dream.router
-       [ Dream.get "/fail" (fun _ -> raise (Failure "The web app failed"))
-       ; Dream.get "/" (fun _ ->
-             Dream.html
-               (Printf.sprintf
-                  "%3i requests successful<br>%3i requests failed"
-                  !successful
-                  !failed))
+       [ Dream.post "/echo" (fun request ->
+             Dream.body request
+             >>= Dream.respond ~headers:[ "Content-Type", "application/octet-stream" ])
        ]
 ;;
