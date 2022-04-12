0de6d6bafa72f8c3ecadf9d00fd7b43dfb98de1b
Jai Verma <jai2.verma@outlook.com>
2022-04-12 16:56:13 +0530

    Adding caqti example with infix syntax

--- bin/main.ml
+++ bin/main.ml
@@ -1,35 +1,119 @@
 open Lwt.Infix
 
-module type DB = Caqti_lwt.CONNECTION
+module Bike = struct
+  type t =
+    { frameno : string
+    ; owner : string
+    ; stolen : Ptime.t option
+    }
+end
 
-module R = Caqti_request
-module T = Caqti_type
+module Q = struct
+  open Caqti_request.Infix
+  open Caqti_type.Std
 
-let list_comments =
-  let query = R.collect T.unit T.(tup2 int string) "SELECT id, text FROM comment" in
-  fun (module Db : DB) -> Db.collect_list query () >>= Caqti_lwt.or_fail
+  let bike =
+    let open Bike in
+    let encode { frameno; owner; stolen } = Ok (frameno, owner, stolen) in
+    let decode (frameno, owner, stolen) = Ok { frameno; owner; stolen } in
+    let rep = Caqti_type.(tup3 string string (option ptime)) in
+    custom ~encode ~decode rep
+  ;;
+
+  let create_bikereg =
+    (unit ->. unit)
+    @@ {eos|
+    CREATE TABLE bikereg (
+      frameno text NOT NULL,
+      owner text NOT NULL,
+      stolen timestamp NULL
+    )
+    |eos}
+  ;;
+
+  let reg_bike =
+    (tup2 string string ->. unit) @@ "INSERT INTO bikereg (frameno, owner) VALUES (?, ?)"
+  ;;
+
+  let report_stolen =
+    (string ->. unit) @@ "UPDATE bikereg SET stolen = current_timestamp WHERE frameno = ?"
+  ;;
+
+  let select_stolen = (unit ->* bike) @@ "SELECT * FROM bikereg WHERE NOT stolen IS NULL"
+  let select_owner = (string ->? string) @@ "SELECT owner FROM bikereg WHERE frameno = ?"
+end
+
+let create_bikereg (module Db : Caqti_lwt.CONNECTION) = Db.exec Q.create_bikereg ()
+
+let reg_bike (module Db : Caqti_lwt.CONNECTION) frameno owner =
+  Db.exec Q.reg_bike (frameno, owner)
 ;;
 
-let add_comment =
-  let query = R.exec T.string "INSERT INTO comment (text) VALUES ($1)" in
-  fun text (module Db : DB) -> Db.exec query text >>= Caqti_lwt.or_fail
+let report_stolen (module Db : Caqti_lwt.CONNECTION) frameno =
+  Db.exec Q.report_stolen frameno
+;;
+
+let find_bike_owner frameno (module Db : Caqti_lwt.CONNECTION) =
+  Db.find_opt Q.select_owner frameno
+;;
+
+let iter_s_stolen (module Db : Caqti_lwt.CONNECTION) f = Db.iter_s Q.select_stolen f ()
+
+let ( >>=? ) m f =
+  m
+  >>= function
+  | Ok x -> f x
+  | Error err -> Lwt.return (Error err)
+;;
+
+let test db =
+  create_bikereg db
+  >>=? fun () ->
+  reg_bike db "BIKE-0000" "Arthur Dent"
+  >>=? fun () ->
+  reg_bike db "BIKE-0001" "Ford Prefect"
+  >>=? fun () ->
+  reg_bike db "BIKE-0002" "Zaphod Beeblebrox"
+  >>=? fun () ->
+  reg_bike db "BIKE-0003" "Trillian"
+  >>=? fun () ->
+  reg_bike db "BIKE-0004" "Marvin"
+  >>=? fun () ->
+  report_stolen db "BIKE-0000"
+  >>=? fun () ->
+  report_stolen db "BIKE-0004"
+  >>=? fun () ->
+  let show_owner frameno =
+    find_bike_owner frameno db
+    >>=? fun owner_opt ->
+    (match owner_opt with
+    | Some owner -> Lwt_io.printf "%s is owned by %s\n" frameno owner
+    | None -> Lwt_io.printf "%s is not registered\n" frameno)
+    >>= Lwt.return_ok
+  in
+  show_owner "BIKE-0003"
+  >>=? fun () ->
+  show_owner "BIKE-0042"
+  >>=? fun () ->
+  Lwt_io.printf "Stolen:"
+  >>= fun () ->
+  iter_s_stolen db (fun bike ->
+      let stolen =
+        match bike.stolen with
+        | Some x -> x
+        | None -> assert false
+      in
+      Lwt_io.printf "\t%s %s %s\n" bike.frameno (Ptime.to_rfc3339 stolen) bike.owner
+      >>= Lwt.return_ok)
+;;
+
+let report_error = function
+  | Ok () -> Lwt.return_unit
+  | Error err -> Lwt_io.eprintl (Caqti_error.show err) >|= fun () -> exit 69
 ;;
 
 let () =
-  Dream.run ~interface:"0.0.0.0"
-  @@ Dream.logger
-  @@ Dream.sql_pool "postgresql://dream:dream@localhost/dream"
-  @@ Dream.sql_sessions
-  @@ Dream.router
-       [ Dream.get "/" (fun request ->
-             Dream.sql request list_comments
-             >>= fun comments -> Dream.html (Render.render comments request))
-       ; Dream.post "/" (fun request ->
-             Dream.form request
-             >>= function
-             | `Ok [ ("text", text) ] ->
-               Dream.sql request (add_comment text)
-               >>= fun () -> Dream.redirect request "/"
-             | _ -> Dream.empty `Bad_Request)
-       ]
+  Lwt_main.run
+    (Caqti_lwt.with_connection (Uri.of_string "postgresql://dream:dream@localhost") test
+    >>= report_error)
 ;;
